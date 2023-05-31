51cc08cddeeee99bc468cee776958c0157b21af9
Ronen-Home <rr4893@pb.amalnet.k12.il>
2023-05-31 17:03:39 +0300

    Added RSA key loading

--- Transperancy_Messenger_Client.cpp
+++ Transperancy_Messenger_Client.cpp
@@ -5,11 +5,62 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
 #include <boost/bind.hpp>
+#include <boost/filesystem.hpp>
+#include <cryptopp/osrng.h>
+#include <cryptopp/files.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/base64.h>
+#include <cryptopp/hex.h>
+//GUI
 
 
+namespace fs = boost::filesystem;
 using namespace std;
 using boost::asio::ip::tcp;
 namespace ssl = boost::asio::ssl;
+
+void generateKeyPair(const std::string& privateKeyPath, const std::string& publicKeyPath)
+{
+    CryptoPP::AutoSeededRandomPool rng;
+
+    CryptoPP::RSA::PrivateKey privateKey;
+    privateKey.GenerateRandomWithKeySize(rng, 2048);
+
+    CryptoPP::RSA::PublicKey publicKey(privateKey);
+
+    CryptoPP::ByteQueue privateKeyBytes;
+    privateKey.Save(privateKeyBytes);
+
+    CryptoPP::ByteQueue publicKeyBytes;
+    publicKey.Save(publicKeyBytes);
+
+    CryptoPP::FileSink privateKeyFile(privateKeyPath.c_str());
+    privateKeyBytes.CopyTo(privateKeyFile);
+
+    CryptoPP::FileSink publicKeyFile(publicKeyPath.c_str());
+    publicKeyBytes.CopyTo(publicKeyFile);
+}
+
+bool checkKeysExist(const std::string& privateKeyPath, const std::string& publicKeyPath)
+{
+    return fs::exists(privateKeyPath) && fs::exists(publicKeyPath);
+}
+
+void loadKeyPair(const std::string& privateKeyPath, const std::string& publicKeyPath,
+    CryptoPP::RSA::PrivateKey& privateKey, CryptoPP::RSA::PublicKey& publicKey)
+{
+    CryptoPP::ByteQueue privateKeyBytes;
+    CryptoPP::FileSource privateKeyFile(privateKeyPath.c_str(), true /*pumpAll*/);
+    privateKeyFile.TransferTo(privateKeyBytes);
+    privateKeyBytes.MessageEnd();
+    privateKey.Load(privateKeyBytes);
+
+    CryptoPP::ByteQueue publicKeyBytes;
+    CryptoPP::FileSource publicKeyFile(publicKeyPath.c_str(), true /*pumpAll*/);
+    publicKeyFile.TransferTo(publicKeyBytes);
+    publicKeyBytes.MessageEnd();
+    publicKey.Load(publicKeyBytes);
+}
 
 void do_read(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& socket, std::array<char, 1024>& read_buffer )
 {
@@ -28,10 +79,10 @@ void do_read(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& socket, std
 
 void do_write(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& socket, std::array<char, 1024>& write_buffer)
 {
-    socket.write_some(boost::asio::buffer(write_buffer)
+    socket.async_write_some(boost::asio::buffer(write_buffer),
 
 
-        /*[write_buffer](const boost::system::error_code& error, size_t length) {
+        [write_buffer](const boost::system::error_code& error, size_t length) {
             if (!error) {
                 cout << "Successfully written: " << write_buffer.data() << endl;
             }
@@ -42,13 +93,46 @@ void do_write(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& socket, st
             }
 
         }
-        */
+        
 
     );
 }
 
 
 int main() {
+    //Key pair protocol
+    
+    const std::string sslFolder = "SSL";
+    const std::string privateKeyPath = sslFolder + "/private.key";
+    const std::string publicKeyPath = sslFolder + "/public.key";
+
+    if (!fs::exists(sslFolder))
+    {
+        fs::create_directory(sslFolder);
+        std::cout << "SSL folder created.\n";
+    }
+    else if (checkKeysExist(privateKeyPath, publicKeyPath))
+    {
+        std::cout << "Keys already exist in the SSL folder.\n";
+        // Load keys and proceed with further operations if needed
+        CryptoPP::RSA::PrivateKey privateKey;
+        CryptoPP::RSA::PublicKey publicKey;
+        loadKeyPair(privateKeyPath, publicKeyPath, privateKey, publicKey);
+
+
+    }
+
+    generateKeyPair(privateKeyPath, publicKeyPath);
+    std::cout << "Key pair generated and saved in the SSL folder.\n";
+
+    // Load keys and proceed with further operations if needed
+    CryptoPP::RSA::PrivateKey privateKey;
+    CryptoPP::RSA::PublicKey publicKey;
+    loadKeyPair(privateKeyPath, publicKeyPath, privateKey, publicKey);
+
+    
+    //SSL protocol
+
     boost::asio::io_context io_context;
     boost::asio::ssl::context ssl_context(boost::asio::ssl::context::tlsv12_client);
 
@@ -89,6 +173,7 @@ int main() {
         std::cout << input << endl;
 
         do_write(socket, myArray);
+        io_context.run();
     }
     
     return 0;
