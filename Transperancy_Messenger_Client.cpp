fd845054ed623926388cc0ab4bc8ba97705905ab
Ronen-Home <rr4893@pb.amalnet.k12.il>
2023-06-10 23:06:18 +0300

    Added context handling

--- Transperancy_Messenger_Client.cpp
+++ Transperancy_Messenger_Client.cpp
@@ -225,6 +225,42 @@ void Clear_Queue(boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& socket,
 
 }
 
+std::string publicKeyToHex(const CryptoPP::RSA::PublicKey& publicKey)
+{
+    std::string encodedPublicKey;
+    CryptoPP::StringSink* sink = new CryptoPP::StringSink(encodedPublicKey);
+
+    // HexEncoder provides the hex encoding functionality
+    CryptoPP::HexEncoder encoder(sink);
+    publicKey.Save(encoder);
+
+    // Finalize the encoding
+    encoder.MessageEnd();
+
+    // Clean up the sink
+    delete sink;
+
+    return encodedPublicKey;
+}
+
+std::string privateKeyToHex(const CryptoPP::RSA::PrivateKey& privateKey)
+{
+    std::string encodedPrivateKey;
+    CryptoPP::StringSink* sink = new CryptoPP::StringSink(encodedPrivateKey);
+
+    // HexEncoder provides the hex encoding functionality
+    CryptoPP::HexEncoder encoder(sink);
+    privateKey.Save(encoder);
+
+    // Finalize the encoding
+    encoder.MessageEnd();
+
+    // Clean up the sink
+    delete sink;
+
+    return encodedPrivateKey;
+}
+
 struct Globals
 {
     boost::asio::ssl::stream<boost::asio::ip::tcp::socket> *socket;
@@ -247,6 +283,27 @@ void atexit_handler()
 }
 
 const int a = atexit(atexit_handler);
+
+void tokenize(vector<string>& vec, string& s, string del)
+{
+    int start, end = -1 * del.size();
+    do {
+        start = end + del.size();
+        end = s.find(del, start);
+        vec.emplace_back(s.substr(start, end - start));
+    } while (end != -1);
+}
+
+void tokenize(vector<string>& vec, const string& s, string del)
+{
+    int start, end = -1 * del.size();
+    do {
+        start = end + del.size();
+        end = s.find(del, start);
+        vec.emplace_back(s.substr(start, end - start));
+    } while (end != -1);
+}
+
 
 int main() {
     //Key pair protocol
@@ -287,9 +344,6 @@ int main() {
     boost::asio::ssl::context ssl_context(boost::asio::ssl::context::tlsv12_client);
     
     globals.io_context = &io_context;
-
-    std::array<char, 1024> read_buffer;
-    std::array<char, 1024> write_buffer;
 
     // Load the self-signed certificate into the SSL context
     ssl_context.load_verify_file("SSL/server_certificate.crt");
@@ -350,8 +404,10 @@ int main() {
     else {
         cout << "JSON file does not exist" << endl;
         // File doesn't exist, generate a new JSON object
+
+        jsonData["uid"] = nullptr;
         jsonData["token"] = nullptr;
-        jsonData["groups"] = json::object();
+        jsonData["groups"] = json::array();
 
         std::cout << jsonData.dump(4) << std::endl;
 
@@ -363,6 +419,201 @@ int main() {
 
     }
 
+    if (!jsonExists)
+    {
+        //Initiate signup
+
+        //initialise a string with hex encoded public key
+        std::string hexPublicKey = publicKeyToHex(publicKey);
+        cout << hexPublicKey << endl;
+
+        //Initialise a SIGNUP request string with the hex encoded public key
+        std::string signupRequest = "SIGNUP " + hexPublicKey + "]";
+
+        //Send the SIGNUP request to the server
+        boost::asio::write(socket, boost::asio::buffer(signupRequest));
+
+        //Read the response from the server
+        std::array<char, 1024> myArray;
+        boost::asio::read(socket, boost::asio::buffer(myArray));
+        std::string response = myArray.data();
+        std::cout << response << std::endl;
+
+        //REFERENCE:
+        //"CODE=200\nSIGNUP SUCCESS\nUID=" + rows[0][0] + " TOKEN=" + std::to_string(token)
+
+        //Parse the response
+        std::vector<std::string> tokens;
+        tokenize(tokens, response, "\n");
+        std::vector<std::string> tokens2;
+        tokenize(tokens2, tokens[2], " ");
+        
+        //Extracts rows[0][0] from UID=rows[0][0]
+        std::string uid = tokens2[0].substr(4, tokens2[0].length() - 4);
+
+        //Extracts token from TOKEN=token
+        std::string token = tokens2[1].substr(6, tokens2[1].length() - 6);
+
+        //Update the JSON object
+        jsonData["token"] = stoi(token);
+        jsonData["uid"] = stoi(uid);
+
+        //Write the JSON object to the file
+        *globals.outputfile << jsonData.dump(4);
+        globals.outputfile->flush();
+
+    }
+    else 
+    {
+        //REFERENCE: "^SIGNIN UID=\\d{1,10} TOKEN=\\d+\\]"
+        //Prepares SIGNIN request
+        std::string signinRequest = "SIGNIN UID=" + std::to_string(jsonData["uid"].get<int>()) + " TOKEN=" + std::to_string(jsonData["token"].get<int>()) + "]";
+
+        //Send the SIGNIN request to the server
+        boost::asio::write(socket, boost::asio::buffer(signinRequest));
+
+        //REFERENCE "CODE=200\nSIGNIN SUCCESS"
+        // "CODE=401\nSIGNIN FAILURE"
+        // "CODE=500\nSIGNIN FAILURE"
+        //Read the response from the server
+        std::array<char, 1024> myArray;
+        boost::asio::read(socket, boost::asio::buffer(myArray));
+
+        //Checks the response code
+        std::string response = myArray.data();
+        std::cout << response << std::endl;
+        std::vector<string> tokens;
+        tokenize(tokens, response, "\n");
+        if (tokens[0] != "CODE=200")
+        {
+			std::cout << "Signin failed" << std::endl;
+			return 1;
+		}
+
+
+        //REFERENCE FOR GETCONTEXT:
+        //"^CONTEXT CURRENT=\\[(?:\\d+,\\d+)(?:&\\d+,\\d+)*\\]"
+        
+        //iterates over all groups and finds their last mesid, while constructing a request string
+        stringstream ss;
+        ss << "GETCONTEXT CURRENT=[";
+
+        //checks that there are groups in the json file
+        //if not, gets context for group 0 from mesid 0
+        if (jsonData["groups"].size() == 0)
+        {
+            ss << "0,0\]";
+        }
+
+        for (const auto& value : jsonData["groups"])
+        {
+			std::string groupid = value["groupid"].get<std::string>();
+			std::string lastmesid = value["lastmesid"].get<std::string>();
+
+			std::string groupidlastmesid = groupid + "," + lastmesid;
+
+			ss << "&" + groupidlastmesid;
+		
+		}
+        ss << "\]";
+
+        //Send the GETCONTEXT request to the server
+        boost::asio::write(socket, boost::asio::buffer(ss.str()));
+        
+        //Read the response from the server
+        std::array<char, 1024> myArray2;
+        boost::asio::read(socket, boost::asio::buffer(myArray2));
+        std::string response2 = myArray2.data();
+        std::cout << response2 << std::endl;
+        //REFERENCE: CODE=200\n<gid>,<mes>,<mes>\n<gid>,<mes>,<mes>&<keyid>,<key_body>\n<keyid>,<key_body>
+        //REFERENCE: CODE=401\nGETCONTEXT FAILURE
+
+        //Tokenises the repsponse by '&' and then by linebreaks
+        std::vector<string> tokens2;
+        tokenize(tokens2, response2, "&");
+        std::vector<string> tokens3;
+        tokenize(tokens3, tokens2[0], "\n");
+        std::vector<string> tokens4;
+        if (tokens2.size() > 1)
+        {
+			tokenize(tokens4, tokens2[1], "\n");
+		}
+
+        //Checks the response code
+        if (tokens3[0] != "CODE=200")
+        {
+            cout << "Getcontext failed" << endl;
+        }
+        else
+        {
+            //For each ine in the response, tokenise by commas, and check if a group fitting it exists.
+            //If not, create a new group and add it to the json file, then append the messages to the group.
+            //Otherwise, append the messages to the group.
+            
+            std::vector<string> tokens6;
+
+            for (auto& group : tokens3)
+            {
+                tokens6.clear();
+				tokenize(tokens6, group, ",");
+				bool groupExists = false;
+                for (auto& value : jsonData["groups"])
+                {
+                    if (value["groupid"] == tokens6[0])
+                    {
+						groupExists = true;
+						break;
+					}
+				}
+                if (!groupExists)
+                {
+					json newGroup;
+					newGroup["groupid"] = tokens6[0];
+					newGroup["lastmesid"] = tokens6[1];
+					newGroup["messages"] = json::array();
+					jsonData["groups"].push_back(newGroup);
+				}
+                for (auto& value : jsonData["groups"])
+                {
+                    if (value["groupid"] == tokens6[0])
+                    {
+						value["lastmesid"] = value["lastmesid"]+ tokens6.size()-1 ;
+                        for (int i = 1; i < tokens6.size(); i++)
+                        {
+							value["messages"].push_back(tokens6[i]);
+						}
+						break;
+					}
+				}
+			}
+
+            //Adds the keys to their respective groups
+            if (tokens2.size() > 1)
+            {
+                for (auto& key : tokens4)
+                {
+					tokens6.clear();
+					tokenize(tokens6, key, ",");
+                    for (auto& value : jsonData["groups"])
+                    {
+                        if (value["groupid"] == tokens6[0])
+                        {
+							value["keyid"] = tokens6[1];
+							value["key_body"] = tokens6[2];
+							break;
+						}
+					}
+				}
+            }
+
+            //Writes the json data to the file
+            *globals.outputfile << jsonData.dump(4);
+            globals.outputfile->flush();
+
+            //Prints the json data to the console
+            cout << jsonData.dump(4) << endl;
+        }
+    }
 
 
     std::thread t2([&m, &consoleQueue]() {console_adaptor(consoleQueue, m); });
